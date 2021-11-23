7e79ead634ae59b636fd5d0503e0ab4cc6b9c9e9
Daniel-Nicolae Ignat <dni957@student.bham.ac.uk>
2021-11-23 21:40:09 +0000

    cleaned the code and added README file

---
+++ README.txt
@@ -0,0 +1,9 @@
+To build and run this application you need to add javafx library to the local project, and update the run configurations by adding VM options, then writing the 
+following command:
+
+--module-path {path_to_javafx_lib_folder} --add-modules javafx.controls,javafx.fxml
+
+where you will change {path_to_javafx_lib_folder} with the path to your local javafx lib folder.
+
+To add the javafx library go to https://gluonhq.com/products/javafx/ and download the sdk coresponding to the operating system in use.
+Extract the files form the zip and place them anywhere you want (once placed you should not move them).
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
-package com.company;
-
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-import static javafx.application.Application.launch;
+
 
 
 public class Main extends Application {
