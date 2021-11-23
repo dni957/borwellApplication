20453330c9f8b9a523479e0243cad629aa5bea7e
Daniel-Nicolae Ignat <dni957@student.bham.ac.uk>
2021-11-23 22:02:44 +0000

    added comments to the code

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,12 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    //Declared the stage and scene variables that allow me to make the UI of the app
+
     Stage window;
     Scene mainScene, introScene;//declaring menu scenes
 
+    //These are the variables used to calculate the area, wall surface and volume of the room
     private double x=0.0,y=0.0,z=0.0;
 
 
@@ -24,34 +27,47 @@ public class Main extends Application {
         launch(args);
     }
 
+
+    //This method transforms the text received as input to be transformed into the right type (double)
     public static double getDoubleFromTextField(TextField textField) {
         String text = textField.getText();
         return Double.parseDouble(text);
     }
 
-    public void closeGame() {
+    //This method is used to stop the code from running after closing the app
+    public void closeApp() {
         window.close();
     }
+
 
     public void start(Stage primaryStage) throws Exception {
 
 
         window = primaryStage;
-        window.setTitle("Menu");
-        window.setOnCloseRequest(e -> closeGame());
 
+        //App title
+        window.setTitle("Dimensions App");
+
+        //Stop the code when the app is closed
+        window.setOnCloseRequest(e -> closeApp());
+
+        //Text box to present which input to be inserted (length of the room)
         Text lengthText = new Text();
         lengthText.setText("Length:");
         lengthText.setFont(Font.font(null, 20));
         lengthText.setTextAlignment(TextAlignment.CENTER);
         lengthText.setTranslateX(10);
 
+
+        //Text box to present which input to be inserted (width of the room)
         Text widthText = new Text();
         widthText.setText("Width:");
         widthText.setFont(Font.font(null, 20));
         widthText.setTextAlignment(TextAlignment.CENTER);
         widthText.setTranslateX(10);
 
+
+        //Text box to present which input to be inserted (height of the room)
         Text heightText = new Text();
         heightText.setText("Height:");
         heightText.setFont(Font.font(null,20));
@@ -59,32 +75,37 @@ public class Main extends Application {
         heightText.setTranslateX(10);
 
 
+        //Input field that receives one of the three dimensions (x dimension)
         TextField xInput = new TextField();
         xInput.setOnAction(e->{
-            x=getDoubleFromTextField(xInput);
+            x=getDoubleFromTextField(xInput);    //action that changes the value of one of the variables used for calculations
         });
         xInput.setMaxWidth(50.0);
         xInput.setTranslateX(100);
         xInput.setTranslateY(-138);
 
 
+        //Input field that receives one of the three dimensions (y dimension)
         TextField yInput = new TextField();
         yInput.setOnAction(e->{
-            y=getDoubleFromTextField(yInput);
+            y=getDoubleFromTextField(yInput);    //action that changes the value of one of the variables used for calculations
         });
         yInput.setMaxWidth(50.0);
         yInput.setTranslateX(100);
         yInput.setTranslateY(-138);
 
 
+        //Input field that receives one of the three dimensions (z dimension)
         TextField zInput = new TextField();
         zInput.setOnAction(e->{
-            z=getDoubleFromTextField(zInput);
+            z=getDoubleFromTextField(zInput);    //action that changes the value of one of the variables used for calculations
         });
         zInput.setMaxWidth(50.0);
         zInput.setTranslateX(100);
         zInput.setTranslateY(-135);
 
+
+        //Output text field for the area of the room
         Text areaOutput = new Text();
         areaOutput.setText("--");
         areaOutput.setFont(Font.font(null,16));
@@ -92,18 +113,18 @@ public class Main extends Application {
         areaOutput.setTranslateX(10);
         areaOutput.setTranslateY(-110);
 
+
+        //Button that shows the output for the area of the room
         Button buttonArea = new Button();
         buttonArea.setText("Area");
         buttonArea.setOnAction(e->{
-            System.out.println("area button pressed");
-            System.out.println(x*y);
-            System.out.println(x);
-            System.out.println(y);
             areaOutput.setText((x*y)+"");
         });
         buttonArea.setTranslateX(10);
         buttonArea.setTranslateY(-100);
 
+
+        //Output text field for the wall surface of the room
         Text wallOutput = new Text();
         wallOutput.setText("--");
         wallOutput.setFont(Font.font(null,16));
@@ -111,6 +132,8 @@ public class Main extends Application {
         wallOutput.setTranslateX(70);
         wallOutput.setTranslateY(-198);
 
+
+        //Button that shows the output for the wall surface of the room
         Button buttonWalls = new Button();
         buttonWalls.setText("Wall surface");
         buttonWalls.setOnAction(e->{
@@ -119,6 +142,8 @@ public class Main extends Application {
         buttonWalls.setTranslateX(70);
         buttonWalls.setTranslateY(-188);
 
+
+        //Output text field for the volume of the room
         Text volumeOutput = new Text();
         volumeOutput.setText("--");
         volumeOutput.setFont(Font.font(null,16));
@@ -126,6 +151,8 @@ public class Main extends Application {
         volumeOutput.setTranslateX(170);
         volumeOutput.setTranslateY(-285);
 
+
+        //Button that shows the output for the volume of the room
         Button buttonVolume = new Button();
         buttonVolume.setText("volume");
         buttonVolume.setOnAction(e->{
@@ -134,6 +161,8 @@ public class Main extends Application {
         buttonVolume.setTranslateX(170);
         buttonVolume.setTranslateY(-275);
 
+
+        //Button used to change the scene to the instructions one
         Button buttonInstructions = new Button();
         buttonInstructions.setText("Back to Instructions");
         buttonInstructions.setOnAction(e->{
@@ -142,21 +171,29 @@ public class Main extends Application {
         buttonInstructions.setTranslateX(60);
         buttonInstructions.setTranslateY(-300);
 
+
+        //Label used as title for the main scene
         Label label = new Label("Room dimensions (m)");
         label.setTranslateX(10);
         label.setFont(Font.font(null,23));
+
+        //Main layout of the app
         VBox mainLayout = new VBox(20);
+
+        //Adding all the elements to the main layout
         mainLayout.getChildren().addAll(label, lengthText,widthText,heightText, xInput, yInput,  zInput,
                 buttonArea, areaOutput, buttonWalls, wallOutput, buttonVolume, volumeOutput, buttonInstructions);
+
+        //Initializing the main scene
         mainScene = new Scene(mainLayout,250, 350);
 
-
+        //Title for the instructions scene
         Label introTitle = new Label();
         introTitle.setText("Instructions:");
         introTitle.setFont(Font.font(null,20));
         introTitle.setTranslateX(10);
 
-
+        //Text that contains the instructions on how to use the app
         Label introText = new Label();
         introText.setText("To use this app, simply input the dimensions of the room by writing the numbers and pressing enter to confirm each entry, then press any of the buttons to receive the output. All of the dimensions are in metres.");
         introText.setWrapText(true);
@@ -164,6 +201,8 @@ public class Main extends Application {
         introText.setMaxWidth(230);
         introText.setTranslateX(10);
 
+
+        //Button used to change to the main scene
         Button introButton = new Button();
         introButton.setText("Next");
         introButton.setOnAction(e->{
@@ -171,14 +210,17 @@ public class Main extends Application {
         });
         introButton.setTranslateX(100);
 
+        //The intro layout
         VBox introLayout = new VBox(20);
+
+        //Adding the elements to the intro layout
         introLayout.getChildren().addAll(introTitle, introText, introButton);
+
+        //Initializing the intro scene
         introScene= new Scene(introLayout,250,350);
 
-
+        //Setting the app window to the intro scene
         window.setScene(introScene);
         window.show();
-
-
     }
 }
